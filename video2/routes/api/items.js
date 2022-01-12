9d722ff6b411bde9f41cc983a023de6d5cd4e9f2
Luca Williams <lucawilliams19@gmail.com>
2022-01-11 19:41:36 -0500

    with delete route

--- video2/routes/api/items.js
+++ video2/routes/api/items.js
@@ -17,7 +17,7 @@ router.get('/', (req, res) => {
 })
 
 //@route POST api/items
-//@desc  create a POST
+//@desc  create a Item
 //access Public
 router.post('/', (req, res) => {
     //create new item lives in the temp memory
@@ -28,6 +28,16 @@ router.post('/', (req, res) => {
     //save new item, now lives in perm storage
     newItem.save().then(item => res.json(item))
 })
+
+//@route DELETE api/items
+//@desc  Delete a item
+//access Public
+router.delete('/:id', (req, res) => {
+    Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({success: true})))
+    .catch(err => res.status(404).json({success: false}))
+})
+
 
 //export the model
 module.exports = router
